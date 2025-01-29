025dc553c90e0e9ca04a31ff6ef95021cc41c19a
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-01-29 10:33:06 +0100

    Test BookRoomTimeIsBefore

--- src/test/java/com/example/BookingSystemTest.java
+++ src/test/java/com/example/BookingSystemTest.java
@@ -8,6 +8,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BookingSystemTest {
@@ -26,30 +27,35 @@ class BookingSystemTest {
 
     @Test
     void BookRoomStartTimeIsNull() {
-        BookingSystem bookingSystem = new BookingSystem(timeProvider, roomRepository, notificationService);
         LocalDateTime endTime = LocalDateTime.now().plusHours(1);
         assertThatThrownBy(() -> bookingSystem.bookRoom("1", null, endTime))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Bokning kräver giltiga start- och sluttider samt rum-id");
-    };
+    }
 
     @Test
     void BookRoomEndTimeIsNull() {
-        BookingSystem bookingSystem = new BookingSystem(timeProvider, roomRepository, notificationService);
         LocalDateTime startTime = LocalDateTime.now().plusHours(1);
         assertThatThrownBy(() -> bookingSystem.bookRoom("1", startTime, null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Bokning kräver giltiga start- och sluttider samt rum-id");
-    };
+    }
 
     @Test
     void BookRoomRoomIdIsNull() {
-        BookingSystem bookingSystem = new BookingSystem(timeProvider, roomRepository, notificationService);
         LocalDateTime startTime = LocalDateTime.now().plusHours(1);
         LocalDateTime endTime = LocalDateTime.now().plusHours(1);
         assertThatThrownBy(() -> bookingSystem.bookRoom(null, startTime, endTime))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Bokning kräver giltiga start- och sluttider samt rum-id");
-    };
+    }
 
+    @Test
+    void BookRoomTimeIsBefore() {
+        LocalDateTime startTime = LocalDateTime.now().minusHours(1);
+        LocalDateTime endTime = LocalDateTime.now();
+        when(timeProvider.getCurrentTime()).thenReturn(LocalDateTime.now());
+        assertThatThrownBy(() -> bookingSystem.bookRoom("1", startTime, endTime))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("Kan inte boka tid i dåtid");
+    }
 }
