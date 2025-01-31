23872cc165f4c7f8c7dadb5235403348f6742d49
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-01-31 09:46:27 +0100

    add methods ShoppingCart, addItem and getItem

--- src/main/java/com/example/ShoppingCart.java
+++ src/main/java/com/example/ShoppingCart.java
@@ -1,4 +1,20 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+    private List<Item> items;
+
+    public ShoppingCart() {
+        this.items = new ArrayList<Item>();
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public List<Item> getItems() {
+        return this.items;
+    }
 }
