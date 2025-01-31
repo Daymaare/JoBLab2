8d329161cb871e5fa8e79e3856eda408a53e2468
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-01-31 09:07:59 +0100

    Setup Mock and InjectMocks

--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -1,7 +1,20 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ShoppingCartTest {
+
+    @Mock
+    private Item item;
+
+    @InjectMocks
+    private ShoppingCart shoppingCart;
 
 }
